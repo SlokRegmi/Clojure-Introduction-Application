e3cfd79a966a6c042955c83b2e18f9d0178cad66
Slok Regmi <slokr86@gmail.com>
2026-04-17 06:19:19 +0545

    Refactor session management to use cookie-store with session secret

--- src/my_playground/core.clj
+++ src/my_playground/core.clj
@@ -3,17 +3,21 @@
             [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.session :refer [wrap-session]]
-            [ring.middleware.session.memory :refer [memory-store]]
+            [ring.middleware.session.cookie :refer [cookie-store]]
             [my-playground.db :as db]
             [my-playground.routes :refer [app-routes]])
   (:gen-class))
+
+(defn- session-secret []
+  (let [s (or (System/getenv "SESSION_SECRET") "dev-secret-change-me-in-prod!!")]
+    (.getBytes s "UTF-8")))
 
 (def app
   (-> app-routes
       (wrap-json-body {:keywords? true})
       wrap-json-response
       wrap-params
-      (wrap-session {:store (memory-store)})))
+      (wrap-session {:store (cookie-store {:key (session-secret)})})))
 
 (defn -main [& _args]
   (db/init!)
